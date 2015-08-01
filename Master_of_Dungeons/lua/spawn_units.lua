ec2bca356532a8d3a7c0451e26b300eb6f198ef4
Michael Babich <michaelbabich7@gmail.com>
2015-08-01 18:17:10 -0400

    Clean up spawn_units.lua

--- Master_of_Dungeons/lua/spawn_units.lua
+++ Master_of_Dungeons/lua/spawn_units.lua
@@ -5,23 +5,23 @@ spawn_units = {}
 -- These are impassable terrains, used to blacklist summoning there.
 spawn_units.bad_terrain = "X*, Q*, *^Xm, Mv"
 
--- Turns a simple table of units or options into one that includes the type.
+-- Turns a simple table of units or options into one that includes the
+-- type.
 local function add_type(units, type)
    local new_table = {}
-
    for i, v in ipairs(units) do
       table.insert(new_table, {v, type})
    end
-
    return new_table
 end
 
--- This goes over all the possible summoners and chooses one with the highest HP in the area.
+-- This goes over all the possible summoners and chooses one with the
+-- highest HP in the area.
 local function find_summoner(x, y, summoners)
    local max_hp = 0
    for key,value in pairs(summoners) do
       if summoners[key].x <= x + 1 and summoners[key].x >= x - 1
-         and summoners[key].y <= y + 1 and summoners[key].y >= y -1
+         and summoners[key].y <= y + 1 and summoners[key].y >= y - 1
          and summoners[key].hitpoints > max_hp then
          max_hp  = summoners[key].hitpoints
          max_key = key
@@ -30,6 +30,8 @@ local function find_summoner(x, y, summoners)
    return summoners[max_key]
 end
 
+-- This spawns a unit with unit_role (summoner type) and icon (image
+-- overlay) as optional arguments.
 local function spawn_unit(x, y, unit_type, side_number, icon, unit_role, traits)
    local unit_stats = {type = unit_type,
                        side = side_number,
@@ -64,59 +66,61 @@ function spawn_units.reg_spawner(unit_type, unit_role, unit_cost)
    end
 end
 
+-- Message menu that lets the Master spawn a summoner of a given
+-- summoner type.
 function spawn_units.menu_boss(type)
    local options = DungeonOpt:new{
       root_message   = "Select a unit to summon.",
       option_message = "&$unit_image~RC(magenta>red)=$input1",
       code           = "spawn_units.boss_spawner('$input1','$input2')"
    }
-
    options.image_string = PORTRAIT[type]
    options:fire(add_type(summoners[type], type))
 end
 
+-- Message menu that lets the summoners spawn a regular unit of a
+-- given summoner type and level.
 function spawn_units.menu_reg(level, type)
    local options = DungeonOpt:new {
       root_message   = "Select a unit to summon.",
       option_message = "&$unit_image~RC(magenta>red)=$input1 - $unit_cost HP",
       code           = "spawn_units.reg_spawner('$input1', '$input2', $unit_cost)"
    }
-
    options.image_string = PORTRAIT[type]
    options:fire(add_type(regular[type][level], type))
 end
 
+-- Message menu that lets the summoners choose a level to select a
+-- unit to summon.
 function spawn_units.menu_reg_levels(type)
    local options = DungeonOpt:new {
       root_message   = "Select a unit level.",
       option_message = "$input1",
       code           = "spawn_units.menu_reg('$input1', '$input2')"
    }
-
    local levels = {}
-
    for key, value in pairs(regular[type]) do
       table.insert(levels, key)
    end
-
    table.sort(levels)
-
    options:fire(add_type(levels, type))
 end
 
+-- Message menu that lets the Master select a summer type so they can
+-- select a summoner.
 function spawn_units.menu_boss_type()
    local options = DungeonOpt:new{
-      root_message   = "Select a unit to summon.",
+      root_message   = "Select a summoner type.",
       image_string   = "portraits/undead/transparent/lich.png",
       option_message = "$input1",
       code           = "spawn_units.menu_boss('$input1')"
    }
-
    options:short_fire(SUMMON_ROLES)
 end
 
+-- Right click menu that lets the Master summon a summoner on
+-- unoccupied, good terrain.
 function spawn_units.menu_item_summon_summoner()
-   -- If they fire up the menu, then the boss selection menu is run.
    wesnoth.fire("set_menu_item", {
                    id          = "005_Summon_Summoner",
                    description = "Summon Summoner",
@@ -146,6 +150,8 @@ function spawn_units.menu_item_summon_summoner()
              )
 end
 
+-- Right click menu that lets a summoner summon a regular unit on
+-- unoccupied, adjacent, good terrain.
 function spawn_units.menu_item_summon(unit_role)
    wesnoth.fire("set_menu_item", {
                    id          = "001_Summon_"..unit_role,
@@ -176,9 +182,14 @@ function spawn_units.menu_item_summon(unit_role)
              )
 end
 
+-- I think this creates a [set_menu_item] for each summoner. Since
+-- this fires on side-turn, maybe it can be optimized by only doing
+-- this for summoners that are on the same side, but then it would
+-- need to handle summoners who change teams to the active team during
+-- the turn. I think this might be the only thing here that actually
+-- needs to run on side_turn.
 function spawn_units.spawn_units()
    spawn_units.menu_item_summon_summoner()
-
    for k, v in pairs(summoners) do
       spawn_units.menu_item_summon(k)
    end
