1e109131b592f286ba98301e5fa61cc17a96c355
Michael Babich <michaelbabich7@gmail.com>
2015-08-01 18:02:48 -0400

    Make spawning code slightly more concise.

--- Master_of_Dungeons/lua/spawn_units.lua
+++ Master_of_Dungeons/lua/spawn_units.lua
@@ -3,7 +3,7 @@
 spawn_units = {}
 
 -- These are impassable terrains, used to blacklist summoning there.
-spawn_units.bad_terrain  = "X*, Q*, *^Xm, Mv"
+spawn_units.bad_terrain = "X*, Q*, *^Xm, Mv"
 
 -- Turns a simple table of units or options into one that includes the type.
 local function add_type(units, type)
@@ -45,27 +45,20 @@ end
 
 function spawn_units.boss_spawner(unit_type, unit_role)
    local e = wesnoth.current.event_context
-   local unit_traits_boss = wesnoth.get_variable("unit_traits_boss")
-   local regenerates      = wesnoth.get_variable("regenerates")
-   spawn_unit(e.x1, e.y1, unit_type, side_number, "misc/hero-icon.png", unit_role, unit_traits_boss)
+   spawn_unit(e.x1, e.y1, unit_type, side_number, "misc/hero-icon.png", unit_role, wesnoth.get_variable("unit_traits_boss"))
+   local regenerates = wesnoth.get_variable("regenerates")
+   local boss_ability = { T["effect"] {
+                             apply_to = "new_ability", {"abilities", regenerates}}}
    local unit = wesnoth.get_unit(e.x1, e.y1)
-   wesnoth.add_modification(unit, "object", {
-                               T["effect"] {
-                                  apply_to = "new_ability", {
-                                     "abilities", regenerates
-                                  }
-                               }
-                            }
-                         )
+   wesnoth.add_modification(unit, "object", boss_ability)
 end
 
 function spawn_units.reg_spawner(unit_type, unit_role, unit_cost)
    local e = wesnoth.current.event_context
-   local unit_traits_reg = wesnoth.get_variable("unit_traits_reg")
    local summoner = find_summoner(e.x1, e.y1, wesnoth.get_units {side = side_number, role = unit_role})
    if summoner.hitpoints > unit_cost then
       summoner.hitpoints = summoner.hitpoints - unit_cost
-      spawn_unit(e.x1, e.y1, unit_type, side_number, false, false, unit_traits_reg)
+      spawn_unit(e.x1, e.y1, unit_type, side_number, false, false, wesnoth.get_variable("unit_traits_reg"))
    else
       wesnoth.message("Error", "Insufficient hitpoints on the attempted summoner.")
    end
