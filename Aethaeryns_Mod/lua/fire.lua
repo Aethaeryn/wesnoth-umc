45ef2f9be09796bde38bf0e64126f8d077965155
Michael Babich <michaelbabich7@gmail.com>
2015-08-06 19:31:12 -0400

    Move functions that use wesnoth.fire to fire.lua

---
+++ Aethaeryns_Mod/lua/fire.lua
@@ -0,0 +1,48 @@
+#define MOD_LUA_FIRE
+<<
+fire = {}
+
+function fire.end_scenario(new_scenario)
+   local scenario = string.format('aeth_mod_%s', new_scenario)
+   wesnoth.fire("endlevel", {
+                   result = "victory",
+                   next_scenario = scenario,
+                   bonus = false,
+                   carryover_add = false,
+                   carryover_percentage = 100 })
+end
+
+function fire.custom_message()
+   wesnoth.fire("message", {
+                   speaker  = "unit",
+                   caption  = "Unit Message",
+                   message  = "What will you say?",
+                   show_for = side_number,
+                   T["text_input"] {
+                      variable  = "aeth_custom_message",
+                      label     = "Type Here:",
+                      max_chars = 50 }})
+   local message = wesnoth.get_variable('aeth_custom_message')
+   if message ~= "" then
+      wesnoth.fire("message", {
+                      side    = side_number,
+                      speaker = "unit",
+                      message = "$aeth_custom_message" })
+   end
+end
+
+function fire.set_menu_item(menu_item_table)
+   wesnoth.fire("set_menu_item", {
+                   id = menu_item_table.id,
+                   description = menu_item_table.text,
+                   image = menu_item_table.image,
+                   menu_item_table.filter,
+                   T["command"] { T["lua"] { code = menu_item_table.command }}})
+end
+
+function fire.clear_menu_item(id)
+   wesnoth.fire("clear_menu_item", { id = id })
+end
+
+>>
+#enddef
--- Aethaeryns_Mod/lua/settings.lua
+++ Aethaeryns_Mod/lua/settings.lua
@@ -1,36 +1,5 @@
 #define MOD_SETTINGS
 <<
-fire = {}
-
-function fire.end_scenario(new_scenario)
-   local scenario = string.format('aeth_mod_%s', new_scenario)
-   wesnoth.fire("endlevel", {
-                   result = "victory",
-                   next_scenario = scenario,
-                   bonus = false,
-                   carryover_add = false,
-                   carryover_percentage = 100 })
-end
-
-function fire.custom_message()
-   wesnoth.fire("message", {
-                   speaker  = "unit",
-                   caption  = "Unit Message",
-                   message  = "What will you say?",
-                   show_for = side_number,
-                   T["text_input"] {
-                      variable  = "aeth_custom_message",
-                      label     = "Type Here:",
-                      max_chars = 50 }})
-   local message = wesnoth.get_variable('aeth_custom_message')
-   if message ~= "" then
-      wesnoth.fire("message", {
-                      side    = side_number,
-                      speaker = "unit",
-                      message = "$aeth_custom_message" })
-   end
-end
-
 function menu_placement()
    local title = "Place Objects"
    local description = "What do you want to do with this unit?"
@@ -278,19 +247,6 @@ function menu_settings()
          feature_toggle("place_object")
       end
    end
-end
-
-function fire.set_menu_item(menu_item_table)
-   wesnoth.fire("set_menu_item", {
-                   id = menu_item_table.id,
-                   description = menu_item_table.text,
-                   image = menu_item_table.image,
-                   menu_item_table.filter,
-                   T["command"] { T["lua"] { code = menu_item_table.command }}})
-end
-
-function fire.clear_menu_item(id)
-   wesnoth.fire("clear_menu_item", { id = id })
 end
 
 local function menu_item_summon(unit_role)
