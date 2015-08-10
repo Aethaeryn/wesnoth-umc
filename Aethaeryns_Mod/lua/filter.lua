75b76c889c4e536e20f82db9cef9b6106816e61f
Michael Babich <michaelbabich7@gmail.com>
2015-08-10 17:57:40 -0400

    Merge the three interact filters.

--- Aethaeryns_Mod/lua/filter.lua
+++ Aethaeryns_Mod/lua/filter.lua
@@ -37,14 +37,28 @@ aeth_mod_filter.summon_summoner = T["show_if"] {
          T["have_unit"] {
             x = "$x1",
             y = "$y1" }}}}
-aeth_mod_filter.interact_unit = T["show_if"] {
+aeth_mod_filter.interact = T["show_if"] {
+   -- menu only shows up on interaction target
    T["have_unit"] {
       x = "$x1",
       y = "$y1",
       T["filter_adjacent"] {
-         side = "$side_number" }}}
-aeth_mod_filter.on_container = T["show_if"] { T["have_unit"] { side = "$side_number", x = "$x1", y = "$y1", T["filter_location"] { x = 5, y = 5 }}}
-aeth_mod_filter.near_container = T["show_if"] { T["have_unit"] { side = "$side_number", x = "$x1", y = "$y1", T["filter_location"] { T["filter_adjacent_location"] { x = 5, y = 5 }}}}
+         side = "$side_number" }},
+   T["or"] {
+      -- menu only shows up on unit at location
+      T["have_unit"] {
+         side = "$side_number",
+         x = "$x1",
+         y = "$y1",
+         T["filter_location"] { x = 5, y = 5 }}},
+   T["or"] {
+      -- menu only shows up on unit adjacent to location
+      T["have_unit"] {
+         side = "$side_number",
+         x = "$x1",
+         y = "$y1",
+         T["filter_location"] {
+            T["filter_adjacent_location"] { x = 5, y = 5 }}}}}
    -- find_in = "$mod_containers"
 
 function aeth_mod_filter.summon(role)
