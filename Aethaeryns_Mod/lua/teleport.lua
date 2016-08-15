27f9a36d3b470dcba166be61e7d1c2e1d0321d3c
Michael Babich <michaelbabich7@gmail.com>
2016-08-14 21:03:04 -0400

    Add more spawns

--- Aethaeryns_Mod/lua/teleport.lua
+++ Aethaeryns_Mod/lua/teleport.lua
@@ -75,18 +75,17 @@ function starter_teleporters()
    add_teleporter( 47, 158, true,  "Blynaerth Teleporter")
    add_teleporter(172,  53, true,  "Rheon Teleporter")
    add_teleporter(204,  31, true,  "Garn Teleporter")
-   add_teleporter(267,  25, true,  "Teleporter")
+   add_teleporter(267,  25, true,  "Duldrasiath Teleporter")
    add_teleporter(210,  55, true,  "Deranion Teleporter")
-   add_teleporter(202,  72, false, "Teleporter")
    add_teleporter(147,  86, true,  "Elabril Teleporter")
-   add_teleporter(202,  72, false, "Teleporter")
-   add_teleporter(141, 122, false, "Teleporter")
-   add_teleporter(238,  64, false, "Teleporter")
+   add_teleporter(202,  72, true,  "Monastery Teleporter")
+   add_teleporter(141, 122, false, "Vargork Teleporter")
+   add_teleporter(238,  64, false, "Teleporter") -- desert
    add_teleporter(300,  79, false, "Temple Teleporter")
-   add_teleporter(310, 144, false, "Teleporter")
-   add_teleporter(214, 116, false, "Teleporter")
+   add_teleporter(310, 144, false, "Teleporter") -- drake
+   add_teleporter(214, 116, false, "Teleporter") -- middle elf town
    add_teleporter(246, 159, false, "Blinadoc Teleporter")
-   add_teleporter(214, 171, false, "Teleporter")
+   add_teleporter(214, 171, false, "Teleporter") -- south elf city
 end
 >>
 #enddef
